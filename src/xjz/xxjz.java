1f5978b0867f52e923a841e4bab53242441303f3
xxxjz2015 <287138737@qq.com>
2015-09-06 14:21:21 +0800

    项目提交

---
+++ src/xjz/xxjz.java
@@ -0,0 +1,9 @@
+package xjz;
+
+public class xxjz {public static void main(String args[]) {
+	  System.out.print("这是我的第一个Java Application 程序！");
+	    System.out.println("运行成功了！");
+	 }
+
+	}
+
---
+++ src/xjz/xxxjz.java
@@ -0,0 +1,19 @@
+package xjz;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class xxxjz {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("ипн╢й╣ож");
+	}
+
+}
